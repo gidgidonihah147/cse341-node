d7fb2c4017d01b31906ad0dc0ca5962748d929fe
TristinParker <96285674+TristinParker@users.noreply.github.com>
2022-09-11 01:42:54 -0600

    Week1 updates

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 const app = express()
 const port = 3000
+
+const uri = "mongodb+srv://Gidgidonihah:Kincaid147@cluster0.miea0ni.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log("DB Connected Successfully");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.use('/',require('./routes'))
 
 app.listen(port, () => {
-    console.log(`example app listening on port ${port}`)
+    console.log(`Server running on port ${port}`)
 })
