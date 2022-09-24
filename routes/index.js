4d65dbc0ec3a9ee0b96d0bd55990f9c7ba2f52b9
TristinParker <96285674+TristinParker@users.noreply.github.com>
2022-09-23 22:30:25 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,7 @@ const routes = require('express').Router();
 
 routes.use('/contacts', require('./contacts'))
 routes.use('/', require('./swagger'));
-routes.use('/',(req, res) => {
-    res.send('Tristin Parker - Base site without a redirect towards http://localhost:3000/contacts or http://localhost:3000/api-docs.');
-});
+
 
 
 module.exports = routes;
