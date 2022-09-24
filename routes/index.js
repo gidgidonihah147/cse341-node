7aba5e81873270d023c949f082e9ff6b7f032069
TristinParker <96285674+TristinParker@users.noreply.github.com>
2022-09-23 22:06:05 -0600

    Corrected index.js
    Added back in the home index hinting to go to contacts or api-docs

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,9 @@ const routes = require('express').Router();
 
 routes.use('/contacts', require('./contacts'))
 routes.use('/', require('./swagger'));
+routes.use('/',(req, res) => {
+    res.send('Tristin Parker - Base site without a redirect towards contacts or api-docs.');
+});
+
 
 module.exports = routes;
